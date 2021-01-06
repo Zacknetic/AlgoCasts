858bf51c4ef892f12b55764425517d6455b615e8
zacknetic <zackavino@gmail.com>
2021-01-06 11:38:16 -0500

    Update index.js

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,31 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, stair = '') {
+    //reached end, so exit
+    if(row === n ){
+        return;
+    }
 
+    if(stair.length + 1 >= n - row && stair.length + 1 <= n + row){
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+//three cases, add a '#', add a ' ', or change to a new row
+//next row
+if(stair.length === (2 * n) - 1){
+    console.log(stair);
+    return pyramid(n, row + 1, '');
+}
+
+pyramid (n, row, stair);
+}
 module.exports = pyramid;
