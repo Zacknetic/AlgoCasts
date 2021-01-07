3b81097666d24ed2bd378ca9fd516a37c7bc8ae0
zacknetic <zackavino@gmail.com>
2021-01-06 19:48:59 -0500

    spiral matrix (non functional)

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -10,11 +10,41 @@
 //     [8, 9, 4],
 //     [7, 6, 5]]
 //  matrix(4)
-//     [[1,   2,  3, 4],
+//     [[1,  2,  3, 4],
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n, row = [0, n], column = [0, n], polarity = 1, count = 1, matrixSpiral = []) {
+    const matrixSize = n * n;
+    if(count === matrixSize){
+        return;
+    }
+    if (polarity > 0){
+        for (row[0] < row[1]; row[0] += polarity;){
+            matrixSpiral[[row[0]][column[0]]] = count;
+            count++;
+        }
+        for (column[0] < column[1]; column[0] += polarity;){
+            matrixSpiral[[row[0]][column[0]]] = count;
+            count++;
+        }
+    } else {
+        for (row[0] > 0; row[0] += polarity;){
+            matrixSpiral[[row[0]][column[0]]] = count;
+            count++;
+        }
+        for (column[0] > 0; column[0] += polarity;){
+            matrixSpiral[[row[0]][column[0]]] = count;
+            count++;
+        }
+        
+    }
+    polarity = polarity * -1;
+    column[1]--;
+    row[1]--;
+
+    matrix(n, row, column,polarity,count + 1,matrixSpiral)
+}
 
 module.exports = matrix;
