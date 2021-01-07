08b72cb5362c1a4be3a88a82e2f679be603d2391
zacknetic <zackavino@gmail.com>
2021-01-06 20:27:07 -0500

    matrix spiral (working)

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,7 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n, row = [0, n], column = [0, n], polarity = 1, count = 1, matrixSpiral = []) {
+function matrix(n){
+    const results = [];
+
+    for (let i = 0; i < n; i++){
+        results.push([])
+    }
+
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startColumn <= endColumn && startRow <= endRow){
+        for(let i = startColumn; i <= endColumn; i++){
+            results[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+        for(let i = startRow; i <= endRow; i++){
+            results[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+
+        for(let i = endColumn; i >= startColumn; i--){
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+        for(let i = endRow; i >= startRow; i--){
+            results[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+    }
+return results;
+}
+
+module.exports = matrix;
+/*
+function matrix(n, row = [0, n - 1], column = [0, n - 1], polarity = 1, count = 1, matrixSpiral = []) {
     const matrixSize = n * n;
     if(count === matrixSize){
         return;
@@ -45,6 +86,4 @@ function matrix(n, row = [0, n], column = [0, n], polarity = 1, count = 1, matri
     row[1]--;
 
     matrix(n, row, column,polarity,count + 1,matrixSpiral)
-}
-
-module.exports = matrix;
+}*/
