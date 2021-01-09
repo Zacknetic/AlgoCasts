8ec397238ff96995873b62c8e5b28e4dca010949
zacknetic <zackavino@gmail.com>
2021-01-09 15:04:49 -0500

    working with nodes

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -9,6 +9,65 @@ class Node {
     }
 }
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data){
+        this.head = new Node(data, this.head);
+    }
+
+    size(){
+        let count = 0;
+        let tempNode = this.head;
+        while(tempNode){
+            count ++;
+            tempNode = tempNode.next;
+        }
+        return count;
+    }
+
+    getFirst(){
+        return this.head;
+    }
+
+    removeFirst(){
+        if(!this.head){
+            return;
+        }
+        this.head = this.head.next;
+    }
+
+    removeLast(){
+        if(!this.head){
+            return;
+        }
+        let tempNode = this.head;
+        for(let i = 0; i < this.size - 1; i++){
+            tempNode = tempNode.next;
+        }
+        tempNode.next = null;
+        
+    }
+    
+
+    getLast(){
+        if(!this.head){
+            return null;
+        }
+        let tempNode = this.head;
+        let lastNode;
+        while(tempNode){
+            lastNode = tempNode;
+            tempNode = tempNode.next;
+        }
+        return lastNode;
+    }
+
+    clear(){
+        this.head = null;
+    }
+}
 
 module.exports = { Node, LinkedList };
